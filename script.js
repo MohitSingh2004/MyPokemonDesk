dccab2c7f0435938ff8a20a94305a72090f5c1a3
MohitSingh2004 <mohitsingh.akoda@gmail.com>
2025-12-06 15:53:13 +0530

    cleanup

--- script.js
+++ script.js
@@ -1,17 +1,102 @@
-// async function ApiCalling() {
-//   let response = await fetch(`https://pokeapi.co/api/v2/pokemon/10`);
-//   let xyz = await response.json();
-//   let imgUrl = xyz.sprites.other.dream_world.front_default;
-//   console.log(imgUrl);
+let warehouse = [];
+let parentContainer = document.getElementById("data-Containers");
+let showMore = document.getElementById("More-button");
+let welcome = document.getElementById("startAnimation");
+let mylogo = document.getElementById("Title");
 
-//   let box = document.getElementById("warGrid");
+let cardsPerTurn = 20;
+let currentcards = 0;
 
-//   let img = document.createElement("img");
-//   img.src = imgUrl;
-//   img.alt = "Pokemon";
+function showWelcome() {
+  mylogo.style.display = "none";
+  welcome.style.display = "block";
+  showMore.style.display = "none";
+}
 
-//   box.appendChild(img);
-//   return xyz;
+function hideWelcome() {
+  Title.style.display = "block";
+  welcome.style.display = "none";
+  showMore.style.display = "block";
+}
+
+async function myPokeApi() {
+  showWelcome();
+  let fetchData = await fetch(
+    `https://pokemon-api-swart-zeta.vercel.app/?id=200`
+  );
+  let jsonData = await fetchData.json();
+  warehouse = jsonData;
+  setTimeout(() => {
+    hideWelcome();
+    ladle();
+  }, 2500);
+}
+
+async function ladle() {
+  if (warehouse.length === 0) {
+    await myPokeApi();
+  }
+  let lastcards = currentcards + cardsPerTurn;
+
+  for (let i = currentcards; i < lastcards && i < warehouse.length; i++) {
+    let childcontainers = document.createElement("div");
+    let img = document.createElement("img");
+    let characterName = document.createElement("h2");
+
+    characterName.innerText = warehouse[i].name;
+    characterName.classList.add("pokemon-name");
+
+    img.src = warehouse[i].image;
+    childcontainers.classList.add("pokemon-box");
+
+    childcontainers.appendChild(img);
+    childcontainers.appendChild(characterName);
+
+    parentContainer.appendChild(childcontainers);
+  }
+  currentcards += cardsPerTurn;
+  if (currentcards >= warehouse.length) {
+    showMore.style.display = "none";
+  }
+}
+showMore.addEventListener("click", ladle);
+myPokeApi();
+
+// let warehouse = [];
+// async function myPokeApi() {
+//   let fetchData = await fetch(
+//     `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0`
+//   );
+//   let jsonData = await fetchData.json();
+//   jsonData.results.map((item) => {
+//     warehouse.push(item.url);
+//   });
 // }
 
-// ApiCalling();
+// async function mainData() {
+//   await myPokeApi();
+//   let Imgwarehouse = [];
+//   let parentContainer = document.getElementById("data-Containers");
+
+//   for (let i = 0; i < warehouse.length; i++) {
+//     let fetchImage = await fetch(warehouse[i]);
+//     let jsonImage = await fetchImage.json();
+//     Imgwarehouse.push(jsonImage.sprites.other.dream_world.front_default);
+//   }
+//   console.log(Imgwarehouse);
+
+//   for (let i = 0; i < Imgwarehouse.length; i++) {
+//     let childcontainers = document.createElement("div");
+//     let img = document.createElement("img");
+
+//     img.src = Imgwarehouse[i];
+//     img.style.width = "200px";
+//     img.style.height = "200px";
+//     childcontainers.appendChild(img);
+//     childcontainers.classList.add("pokemon-box");
+//     parentContainer.appendChild(childcontainers);
+//   }
+// }
+// mainData().catch((error) => {
+//   console.log("error", error);
+// });
