a05cd7d7f962b47b836b7a9197e842bccb36ca3c
MohitSingh2004 <mohitsingh.akoda@gmail.com>
2025-12-06 15:42:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// async function ApiCalling() {
+//   let response = await fetch(`https://pokeapi.co/api/v2/pokemon/10`);
+//   let xyz = await response.json();
+//   let imgUrl = xyz.sprites.other.dream_world.front_default;
+//   console.log(imgUrl);
+
+//   let box = document.getElementById("warGrid");
+
+//   let img = document.createElement("img");
+//   img.src = imgUrl;
+//   img.alt = "Pokemon";
+
+//   box.appendChild(img);
+//   return xyz;
+// }
+
+// ApiCalling();
